00de99540864fc7f4ab1ff9209fb52254617c1c0
faboussa <boussard.fanny@gmail.com>
2024-10-21 19:03:07 +0200

    changer Colors.hpp to colors.hpp - running loop

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,61 +1,81 @@
 #include "Server.hpp"
+#include "../includes/colors.hpp"
 
 bool Server::_signal = false;
 
-Server::Server(int port) : _port(port){
+Server::Server(int port) : _port(port) {
     _signal = false;
     _socketFd = -1;
 }
 
-void Server::runServer(){
-
-    std::cout << "Server running" << std::endl;
+void Server::runServer() {
+    createSocket();
+    std::cout << GREEN << "Server started on port " << _port << RESET << std::endl;
+     while (!_signal) {
+        int pollCount = poll(&_pollFds[0], _pollFds.size(), -1);
+    }
+    std::cout << RED << "Server shutting down..." << RESET << std::endl;
 }
 
-void Server::closeServer(){
-    if (_socketFd != -1)
-    {
+void Server::closeServer() {
+    for (size_t i = 0; i < _clients.size(); i++) {
+        std::cout << RED << "Client <" << _clients[i].getFd() << "> Disconnected" << RESET << std::endl;
+        close(_clients[i].getFd());
+    }
+    if (_socketFd != -1) {
+        std::cout << RED << "Server <" << _socketFd << "> Disconnected" << RESET << std::endl;
         close(_socketFd);
-        std::cout << "Server closed" << std::endl;
     }
 }
 
-void Server::signalHandler(int signal){
-    if (signal == SIGINT || signal == SIGQUIT)
-    {
+void Server::signalHandler(int signal) {
+    if (signal == SIGINT || signal == SIGQUIT) {
+        std::cout << std::endl << "Signal Received!" << std::endl;
         Server::_signal = true;
     }
 }
 
-//revoir
-void Server::clearClients(int fd){ //-> clear the clients
-	for(size_t i = 0; i < _poll_fds.size(); i++){ //-> remove the client from the pollfd
-		if (_poll_fds[i].fd == fd)
-			{_poll_fds.erase(_poll_fds.begin() + i); break;}
-	}
-	for(size_t i = 0; i < _clients.size(); i++){ //-> remove the client from the vector of clients
-		if (_socketFd == fd)
-			{_clients.erase(_clients.begin() + i); break;}
-	}
-
+void Server::clearClients(int fd) {
+    for(size_t i = 0; i < _pollFds.size(); i++) {
+        if (_pollFds[i].fd == fd) {
+            _pollFds.erase(_pollFds.begin() + i);
+            break;
+        }
+    }
+    for(size_t i = 0; i < _clients.size(); i++) {
+        if (_clients[i].getFd() == fd) {
+            _clients.erase(_clients.begin() + i);
+            break;
+        }
+    }
 }
 
-void Server::createSocket(){
-    _socketFd = socket(AF_INET, SOCK_STREAM, 0); // ipv4, tcp, 0
-    if (_socketFd == -1)
-    {
+void Server::createSocket() {
+    _address.sin_family = AF_INET;
+    _address.sin_addr.s_addr = INADDR_ANY;
+    _address.sin_port = htons(_port);
+
+    _socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (_socketFd == -1) {
         throw std::runtime_error("Failed to create socket");
     }
-    _address.sin_family = AF_INET; //An integer representing the address family. For IPv4
-    _address.sin_addr.s_addr = INADDR_ANY; // It represents “any” IP address, meaning the socket will be bound to all available network interfaces on the host.
-    _address.sin_port = htons(_port); //converting a 16-bit unsigned short integer from host byte order to network byte order.
+
+    int en = 1;
+    if (setsockopt(_socketFd, SOL_SOCKET, SO_REUSEADDR, &en, sizeof(en)) == -1)
+        throw std::runtime_error("Failed to set option SO_REUSEADDR on socket");
+
+    if (fcntl(_socketFd, F_SETFL, O_NONBLOCK) == -1)
+        throw std::runtime_error("Failed to set option O_NONBLOCK on socket");
+
     if (bind(_socketFd, (struct sockaddr *)&_address, sizeof(_address)) == -1)
-    {
         throw std::runtime_error("Failed to bind socket");
-    }
-    if (listen(_socketFd, 10) == -1) // int listen(int sockfd, int backlog);
-    {
+
+    if (listen(_socketFd, 10) == -1)
         throw std::runtime_error("Failed to listen on socket");
-    }
-   _poll_fds.push_back({_socketFd, POLLIN, 0}); //  fill the struct pollfd with the server information. To make the server ready to receive incoming connections, set the events argument to POLLIN and push the struct to the vector fds.
+
+    struct pollfd newPoll;
+    newPoll.fd = _socketFd;
+    newPoll.events = POLLIN;
+    newPoll.revents = 0;
+    _pollFds.push_back(newPoll);
 }
