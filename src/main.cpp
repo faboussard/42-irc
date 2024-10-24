2026bcfe143105e1cd8d3627a958b812c942635f
faboussa <boussard.fanny@gmail.com>
2024-10-24 10:55:13 +0200

    format: removed red from the usage message

--- src/main.cpp
+++ src/main.cpp
@@ -14,19 +14,19 @@
 #include "../includes/colors.hpp"
 
 void checkArgs(int port, const std::string& password) {
-  if (port < 1080 || port > 65535) {
-    std::cerr << RED "Invalid port number" RESET << std::endl;
+  if (port <= 1080 || port > 65535) {
+    std::cerr << "Invalid port number (ports accepted : 1081 to 65535)" << std::endl;
     exit(EXIT_FAILURE);
   }
   if (password.length() > 1000) {
-    std::cerr << RED "Password too long" RESET << std::endl;
+    std::cerr << "Password too long" << std::endl;
     exit(EXIT_FAILURE);
   }
 }
 
 int main(int ac, char** argv) {
   if (ac != 3) {
-    std::cerr << RED "Usage: ./ircserv <port> <password>" RESET << std::endl;
+    std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
     exit(EXIT_FAILURE);
   }
 
