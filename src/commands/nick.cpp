1dca6821698fac3cec3a056f8eb3dfc956439474
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2024-11-05 11:49:26 +0100

    fix: NICK no longers shows an error when we put our own nickname to replace our nickname

--- src/commands/nick.cpp
+++ src/commands/nick.cpp
@@ -6,7 +6,7 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 09:46:04 by mbernard          #+#    #+#             */
-/*   Updated: 2024/11/05 11:30:55 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/11/05 11:45:02 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,14 @@ bool Parser::verifyNick(std::string nick, Client& client, clientsMap cltMap) {
   }
   clientsMap::iterator itEnd = cltMap.end();
   for (clientsMap::iterator it = cltMap.begin(); it != itEnd; ++it) {
-    if (it->second.getNickname() == nick) {
-      send432ErroneusNickname(client);
+    if (it->second.getNickname() == nick
+        && it->second.getFd() != client.getFd()) {
+      send433NickAlreadyInUse(client);
       return (false);
     }
   }
   client.setNickname(nick);
-  std::cout << BRIGHT_YELLOW "NickName IS ACCEPTED !!!!! : " << client.getNickname() << RESET << std::endl;
+  std::cout << BRIGHT_YELLOW "NickName IS ACCEPTED !!!!! : "
+            << client.getNickname() << RESET << std::endl;
   return (true);
 }
