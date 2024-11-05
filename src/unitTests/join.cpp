9d2bf107977c233680db48cda4a75578e6ada40d
faboussa <boussard.fanny@gmail.com>
2024-11-05 11:53:29 +0100

    TEST - colors added

--- src/unitTests/join.cpp
+++ src/unitTests/join.cpp
@@ -6,15 +6,16 @@
 /*   By: faboussa <faboussa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 11:50:56 by mbernard          #+#    #+#             */
-/*   Updated: 2024/11/05 11:42:01 by faboussa         ###   ########.fr       */
+/*   Updated: 2024/11/05 11:53:07 by faboussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <map>
-#include <string>
-#include "Server.hpp" 
-#include "tests.hpp"
+
+#include "../includes/Server.hpp"
+#include "../includes/colors.hpp"
+#include "../includes/numericReplies.hpp"
+#include "../includes/utils.hpp"
+#include "../includes/tests.hpp"
 
 void Test::testJoinChannel(Server &server) {
     
@@ -32,41 +33,41 @@ void Test::testJoinChannel(Server &server) {
     std::string channel3 = "#channelC";
 
     // Test de l'entrée d'un client dans un canal
-    std::cout << "Testing client joining channel: " << channel1 << std::endl;
+    std::cout << MAGENTA "Testing client joining channel: " RESET << channel1 << std::endl;
     server.joinChannel(channel1, fd1);
 
     // Test de l'entrée du deuxième client dans le même canal
-    std::cout << "Testing second client joining the same channel: " << channel1 << std::endl;
+    std::cout << MAGENTA "Testing second client joining the same channel: " RESET << channel1 << std::endl;
     server.joinChannel(channel1, fd2);
 
     // Test de l'entrée du premier client dans un autre canal
-    std::cout << "Testing first client joining another channel: " << channel2 << std::endl;
+    std::cout << MAGENTA "Testing first client joining another channel: " RESET << channel2 << std::endl;
     server.joinChannel(channel2, fd1);
 
     // Test de la limite de canaux si une limite est implémentée (par exemple, 2 canaux max)
-    std::cout << "Testing channel limit" << std::endl;
+    std::cout << MAGENTA "Testing channel limit" RESET << std::endl;
     std::cout << "Chanlimit is " << CHANLIMIT_ << std::endl;
     std::cout << "fd1 is in " << server.getClientByFd(fd1).getChannelsCount() << " channels" << std::endl;
     server.joinChannel(channel3, fd1);  // Cette tentative doit être rejetée si le nombre de canaux est limité
     std::cout << "fd1 is in " << server.getClientByFd(fd1).getChannelsCount() << " channels" << std::endl;
 
     // Test de JOIN 0 pour quitter tous les canaux
-    std::cout << "Testing JOIN 0 to leave all channels" << std::endl;
+    std::cout << MAGENTA "Testing JOIN 0 to leave all channels" RESET << std::endl;
     std::string leaveAll = "0";
     server.joinChannel(leaveAll, fd1);  // Simule une sortie de tous les canaux
 
     // bad param
-    std::cout << "Testing JOIN bad prefix" << std::endl;
+    std::cout << MAGENTA "Testing JOIN bad prefix" RESET << std::endl;
     std::string badParam = "&ssssss";
     server.joinChannel(badParam, fd1); 
 
     // no param
-    std::cout << "Testing JOIN no prefix" << std::endl;
+    std::cout << MAGENTA "Testing JOIN no prefix" RESET << std::endl;
     std::string noprefix = "g";
     server.joinChannel(noprefix, fd1); 
 
     // empty param
-    std::cout << "Testing JOIN empty param" << std::endl;
+    std::cout << MAGENTA "Testing JOIN empty param" RESET << std::endl;
     std::string empty = "";
     server.joinChannel(empty, fd1); 
 
