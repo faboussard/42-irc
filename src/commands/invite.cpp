9ef7ff1d6cee037e3c6f33f80f0f064c2d5da4c8
yusengok <ysengoku.tourisme@gmail.com>
2024-11-14 12:19:35 +0100

    fix: Send channel list that the client was invited to

--- src/commands/invite.cpp
+++ src/commands/invite.cpp
@@ -6,44 +6,46 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 14:48:41 by yusengok          #+#    #+#             */
-/*   Updated: 2024/11/14 08:28:28 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/11/14 12:17:33 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/Server.hpp"
 
 // Commands
-// INVITE clientNick #42Channel ------> Invite clientNick to #42Channel
-// INVITE ------> Indicate a channel the client was invited to
+// INVITE clientNick #42Channel
+// ------> Invite clientNick to #42Channel
+// INVITE
+// ------> Indicate a channel the client was invited to (impossible on Hexchat)
 
-void Server::invite(int invitingClientFd, const std::string &arg) {
+void Server::invite(int fd, const std::string &arg) {
   if (arg.empty()) {
-    sendInvitedChannelsList(invitingClientFd);
+    sendInvitedChannelsList(fd);
     return;
   }
   std::istringstream iss(arg);
-  std::string nick, channelNameWithPrefix;
-  iss >> nick >> channelNameWithPrefix;
-  if (!inviteParamsValid(invitingClientFd, nick, channelNameWithPrefix))
+  std::string invitedNick, channelName;
+  iss >> invitedNick >> channelName;
+  if (!inviteParamsValid(fd, invitedNick, channelName))
     return;
   else
-    inviteClientToChannel(invitingClientFd, nick, channelNameWithPrefix);
+    inviteClientToChannel(fd, invitedNick, channelName);
 }
 
 bool Server::inviteParamsValid(int invitingClientFd,
                                const std::string &invitedNick,
-                               const std::string &channelNameWithPrefix) {
-  if (channelNameWithPrefix.empty()) {
+                               const std::string &channelName) {
+  if (channelName.empty()) {
     send461NeedMoreParams(_clients.at(invitingClientFd), "INVITE");
     return (false);
   } else if (!nickExists(invitedNick)) {
     send401NoSuchNick(_clients.at(invitingClientFd), invitedNick);
     return (false);
-  } else if (channelNameWithPrefix[0] != REG_CHAN) {
-    send476BadChanMask(_clients.at(invitingClientFd), channelNameWithPrefix);
+  } else if (channelName[0] != REG_CHAN) {
+    send476BadChanMask(_clients.at(invitingClientFd), channelName);
     return (false);
-  } else if (!channelExists(channelNameWithPrefix)) {
-    send403NoSuchChannel(_clients.at(invitingClientFd), channelNameWithPrefix);
+  } else if (!channelExists(channelName)) {
+    send403NoSuchChannel(_clients.at(invitingClientFd), channelName);
     return (false);
   }
   return (true);
@@ -51,13 +53,12 @@ bool Server::inviteParamsValid(int invitingClientFd,
 
 void Server::inviteClientToChannel(int invitingClientFd,
                                    const std::string &invitedNick,
-                                   const std::string &channelNameWithPrefix) {
-  Channel &channel = _channels.at(channelNameWithPrefix.substr(1));
+                                   const std::string &channelName) {
+  Channel &channel = _channels.at(channelName.substr(1));
   Client *invitedClient;
   clientsMap::const_iterator itEnd = _clients.end();
   for (clientsMap::iterator it = _clients.begin(); it != itEnd; ++it) {
-    if (it->second.getNickname() == invitedNick)
-      invitedClient = &it->second;
+    if (it->second.getNickname() == invitedNick) invitedClient = &it->second;
   }
   if (!channel.isClientInChannel(invitingClientFd)) {
     send442NotOnChannel(_clients.at(invitingClientFd), channel);
@@ -78,47 +79,13 @@ void Server::inviteClientToChannel(int invitingClientFd,
   }
 }
 
-/// send to invited client list
 void Server::sendInvitedChannelsList(int fd) const {
   const Client &client = _clients.at(fd);
   channelsMap::const_iterator itEnd = _channels.end();
   for (channelsMap::const_iterator it = _channels.begin(); it != itEnd; ++it) {
-    if (it->second.isClientInChannel(fd)) {
+    if (it->second.isClientInvited(fd)) {
       send336Invitelist(client, it->second);
     }
   }
   send337Endofinvitelist(client);
 }
-
-// ======= INVITE <nick of the person to be invited> <channel name> ============
-// Replies:
-// -- 341 RPL_INVITING:
-// Confirms the user has been invited to the channel.
-
-// -- 403 ERR_NOSUCHCHANNEL:
-// The channel does not exist.
-
-// -- 442 ERR_NOTONCHANNEL:
-// Only members of the channel are allowed to invite other users.
-// Otherwise, the server MUST reject the command with 442.
-
-// -- 443 ERR_USERONCHANNEL:
-// The user is already on the target channel, the server MUST reject with 443.
-
-// -- 482 ERR_CHANOPRIVSNEEDED:
-// The user is not a channel operator.
-
-// ======== INVITE (without parameters) ========================================
-// Replies:
-// -- 336 RPL_INVITELIST:
-// Indicate a channel the client was invited to.
-// If the client is invited to multiple channels,
-// the server sends multiple replies.
-// If there is no channel to which the client was invited,
-// the server sends only 337.
-
-// -- 337 RPL_ENDOFINVITELIST:
-
-// ======= INVITE (without not enough parameters) ==============================
-// Replies:
-// -- 461 ERR_NEEDMOREPARAMS:
